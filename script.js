afec53671e53c03ef1d0726ade9eb8ccb7ae823b
hamzalachguer <lachguerhmza@gmail.com>
2025-08-13 00:18:34 +0200

    Uploading the starter files

---
+++ script.js
@@ -0,0 +1,45 @@
+// IMPORTING
+
+
+/* 
+  *** HEADER CODE ***
+  
+*/
+
+// Caching DOM elements
+
+// Toggle navigation menu function
+// Menu button eventListener
+
+
+
+
+
+
+
+
+/* 
+  *** SLIDER CODE ***
+  
+*/
+
+// Caching DOM elements
+// Generate content | Dot on click
+// Next slide
+// Previous slide
+// Update slider
+
+
+
+
+
+
+
+/* 
+  *** THE MOVIE GRID | FETCHING API ***
+  
+*/
+
+// FETCHING API
+
+
