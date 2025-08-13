60e786340a503020117b4f13876a54c2345fe357
hamzalachguer <lachguerhmza@gmail.com>
2025-08-13 12:27:33 +0200

    Creating a dynamic header and an auto sliding hero

---
+++ data.js
@@ -0,0 +1,15 @@
+export const slidesData = [
+  {
+    id: "0",
+    back_drop: "https://image.tmdb.org/t/p/original/vJerZdFTbPwNjKmAcSipd3jkBwW.jpg",
+    log: "https://image.tmdb.org/t/p/original/jdAWFEuysoBGkzB1MOnHr9qqqqP.png",
+    overview: "Taking place during the events of John Wick: Chapter 3 – Parabellum, Eve Macarro begins her training in the assassin traditions of the Ruska Roma.",
+    slogan: "Vengeance has a new face."
+  },  {
+    id: "1",
+    back_drop: "https://image.tmdb.org/t/p/original/iLdwdj3X0NCgUGZqfIPHFI3pzHv.jpg",
+    log: "https://image.tmdb.org/t/p/original/cCf1pAVXhKNXMgqp8sbZj36CEK5.png",
+    overview: "In a society devastated by the effects of an all-out war between heroes and villains, a mysterious giant fortress suddenly appears, engulfing towns and people one after another. Then, a man reminiscent of All Might, the 'symbol of peace', stands in front of Izuku and his friends...",
+    slogan: "Next up is you."
+  }
+]
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // IMPORTING
+import {slidesData} from './data.js';
 
 
 /* 
@@ -7,10 +8,26 @@
 */
 
 // Caching DOM elements
+const menuBtn = document.querySelector("#toggle-menu-btn");
+const navMenu = document.querySelector("#nav-menu");
+const header = document.querySelector("header");
+
+let isMenuOpen = false;
 
 // Toggle navigation menu function
-// Menu button eventListener
+function toggleNavigationMenu() {
+  if (!isMenuOpen) {
+    menuBtn.classList.toggle("closed")
+    navMenu.classList.toggle("hide-menu");
+  }
 
+  // update aria attributes
+  menuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  navMenu.setAttribute("aria-hidden", isMenuOpen);
+}
+
+// Menu button eventListener
+menuBtn.addEventListener('click', toggleNavigationMenu)
 
 
 
@@ -24,14 +41,102 @@
 */
 
 // Caching DOM elements
+const slideContainer = document.querySelector("#slide-data");
+const next = document.querySelector("#next-slide");
+const prev = document.querySelector("#previous-slide");
+const sliderDots = document.querySelector("#slider-dots");
+
+
+
 // Generate content | Dot on click
+function renderSliderDots() {
+  slidesData.forEach((_, index) => {
+    const button = document.createElement("button");
+    button.setAttribute("data-slide-id", index);
+    button.classList = "h-2 w-2 transition-all ease-in-out duration-600 bg-[#ffffff50] rounded-2xl";
+
+    if (index === 0) {
+      button.classList.add("bg-white");
+      button.classList.add("w-[20px]");
+    }
+
+    sliderDots.append(button);
+    button.addEventListener('click', () => {
+      updateSlider(index);
+    })
+  })
+}
+
+
+let currentSlide = 0;
+updateSlider(currentSlide)
+
 // Next slide
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slidesData.length;
+  updateSlider(currentSlide)
+  console.log("next")
+}
+
 // Previous slide
+function prevSlide() {
+  currentSlide = (slidesData.length + (currentSlide - 1)) % slidesData.length;
+  updateSlider(currentSlide)
+  console.log("previous")
+}
+
 // Update slider
+function updateSlider(currentSlide) {
+  const slideData = slidesData[currentSlide];
+
+  slideContainer.innerHTML = `
+  <div class="h-screen overflow-hidden">
+    <img class="w-full h-full object-cover object-center animate-[opacity_1s_ease-in-out]" src=${slideData.back_drop} alt="Ballerina (2025)" loading="lazy">
+  </div> 
+
+  <div id="hero-content" class="text-white flex flex-col gap-5 absolute z-[8888] bottom-9 left-0 p-5">
+
+    <div class="h-[100px] w-full overflow-hidden">
+      <img class="h-full object-cover object-center animate-[heroSlider_1s_ease-in-out]" src=${slideData.log} alt="" loading="lazy">
+    </div>
+
+    <div class="flex flex-col gap-5">
+      <p class="max-w-[560px] animate-[heroSlider_1.5s_ease-in-out]">${slideData.overview}</p>
+
+      <div class="flex flex-col gap-5 md:flex-row md:items-center  animate-[heroSlider_2s_ease-in-out]">
+        <button class="w-fit flex items-center text-[15px] text-black gap-[10px] px-4 capitalize font-medium py-2 bg-white rounded-3xl">
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 4.98951C5 4.01835 5 3.53277 5.20249 3.2651C5.37889 3.03191 5.64852 2.88761 5.9404 2.87018C6.27544 2.85017 6.67946 3.11953 7.48752 3.65823L18.0031 10.6686C18.6708 11.1137 19.0046 11.3363 19.1209 11.6168C19.2227 11.8621 19.2227 12.1377 19.1209 12.383C19.0046 12.6635 18.6708 12.886 18.0031 13.3312L7.48752 20.3415C6.67946 20.8802 6.27544 21.1496 5.9404 21.1296C5.64852 21.1122 5.37889 20.9679 5.20249 20.7347C5 20.467 5 19.9814 5 19.0103V4.98951Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          watch now
+        </button>
+        <p class="text-yellow-500">${slideData.slogan}</p>
+      </div>
+    </div>
+  </div>
+  `
+
+  sliderDots.querySelectorAll("button")
+    .forEach((dot, index) => {
+      dot.classList.toggle("bg-white", index === currentSlide);
+      dot.classList.toggle("w-[20px]", index === currentSlide);
+    })
+}
+// set intervale each 3s
+let interval = setInterval(() => {
+  
+  nextSlide()
+  // clearInterval(interval);
+}, 5000)
 
 
+function initSlider() {
+  renderSliderDots()
+  next.addEventListener('click', nextSlide);
+  prev.addEventListener('click', prevSlide);
+}
 
-
+initSlider()
 
 
 
@@ -39,6 +144,8 @@
   *** THE MOVIE GRID | FETCHING API ***
   
 */
+
+const API_KEY = "bb28b6615bf6d0de7b544cd679643b59";
 
 // FETCHING API
 
